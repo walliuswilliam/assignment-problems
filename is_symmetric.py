15f6e94354b92bdcac8f0bae0a4c7b2a61bc849e
walliuswilliam <william.wallius@gmail.com>
2020-08-21 18:50:46 +0000

    new symmetry check

--- is_symmetric.py
+++ is_symmetric.py
@@ -1,1 +1,9 @@
-def is_symmetric(input_string)
+def is_symmetric(input_string):
+  if input_string == input_string[::-1]:
+    symmetry_check = True
+  else:
+    symmetry_check = False
+  return symmetry_check
+
+print(is_symmetric("racecar"))
+print(is_symmetric("batman"))
