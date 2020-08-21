c7c75d9d06b7fc91a8eebe15393e568a5d2dc28a
walliuswilliam <william.wallius@gmail.com>
2020-08-21 19:42:26 +0000

    if statements + print

--- letters_numbers_conversion.py
+++ letters_numbers_conversion.py
@@ -1,5 +1,9 @@
 def convert_to_numbers(input_string):
   an_array = []
-for char in input_string:
-  if char != ' ':
-    an_array.append(ord(char) - 64)
+  for char in input_string:
+    if char != ' ':
+      an_array.append(ord(char) - 9)
+    if char == ' ':
+      an_array.append(0)
+  return an_array
+print(convert_to_numbers('a cat'))
