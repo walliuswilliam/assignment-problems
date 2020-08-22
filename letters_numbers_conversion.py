4a562401529bb7c544883c061876d4a48633e858
walliuswilliam <william.wallius@gmail.com>
2020-08-22 17:37:40 +0000

    convert to numbers

--- letters_numbers_conversion.py
+++ letters_numbers_conversion.py
@@ -2,8 +2,11 @@ def convert_to_numbers(input_string):
   an_array = []
   for char in input_string:
     if char != ' ':
-      an_array.append(ord(char) - 9)
+      an_array.append(ord(char) - 96) #found method from Remy, he explained how to do it
     if char == ' ':
       an_array.append(0)
   return an_array
-print(convert_to_numbers('a cat'))
+
+assert convert_to_numbers('a cat') == [1, 0, 3, 1, 20], "test failed"
+print('PASSED')
+
