ac6ec67d70e66ecc26160a9a7ca506e813ea3d75
walliuswilliam <william.wallius@gmail.com>
2020-08-22 17:52:00 +0000

    letters numbers done

--- letters_numbers_conversion.py
+++ letters_numbers_conversion.py
@@ -2,11 +2,24 @@ def convert_to_numbers(input_string):
   an_array = []
   for char in input_string:
     if char != ' ':
-      an_array.append(ord(char) - 96) #found method from Remy, he explained how to do it
+      an_array.append(ord(char) - 96)
     if char == ' ':
       an_array.append(0)
   return an_array
 
-assert convert_to_numbers('a cat') == [1, 0, 3, 1, 20], "test failed"
+print('converting "batman" to numbers...')
+assert convert_to_numbers('batman') == [2, 1, 20, 13, 1, 14], "test failed"
 print('PASSED')
 
+def convert_to_letters(input_string):
+  a_string = ""
+  for int in input_string:
+    if int != 0:
+      a_string += chr(int + 96)
+    if int == 0:
+      a_string += (" ")
+  return a_string
+
+print('converting [1, 0, 3, 1, 20] to letters...')
+assert convert_to_letters([1, 0, 3, 1, 20]) == 'a cat', "test failed"
+print('PASSED')
