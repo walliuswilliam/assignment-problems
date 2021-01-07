f36b724b74f8357ead0ff14ac48ab59817ea6e93
walliuswilliam <william.wallius@gmail.com>
2021-01-07 00:55:24 +0000

    working cumulative distribution

--- 1Assignment 43/random_draw.py
+++ 1Assignment 43/random_draw.py
@@ -1,11 +1,15 @@
 import random as rand
 
+def cumulative_distribution(input_distribution):
+  new_dist = []
+  cumul_sum = 0
+  for item in input_distribution:
+    cumul_sum += item
+    new_dist.append(cumul_sum)
+  return new_dist
+
 def random_draw(distribution):
-  cumul_dist = []
-  for item in range(len(distribution)):
-    if item = 0:
-      cumul_dist.append(distribution[item])  
-    else:
-      cumul_dist.append(distribution[item]+distribution[item-1])
+  cumul_dist = cumulative_distribution(distribution)
   return cumul_dist
 
+print(random_draw([0.05, 0.2, 0.15, 0.3, 0.1, 0.2]))
